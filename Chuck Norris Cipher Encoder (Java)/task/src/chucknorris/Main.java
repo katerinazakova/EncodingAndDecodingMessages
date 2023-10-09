80bdd908a8905e166614f5f708ec2eda78684ba2
7ZKaty <zakova.kz@gmail.com>
2023-10-09 11:22:28 +0200

    Stage 5 - correction of methods: is0Or00InEvenBlocks, decodeInputMessage. Creating new method decodingBinaryString.

--- Chuck Norris Cipher Encoder (Java)/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder (Java)/task/src/chucknorris/Main.java
@@ -21,7 +21,7 @@ public class Main {
                     System.out.println("Input encoded string:");
                     String inputMessage = scanner.nextLine();
                     if (isOnlyZeroOrSpaceInCode(inputMessage)
-                            && isNumberOfBlockEven(inputMessage)
+                            && isNumberOfBlocksEven(inputMessage)
                             && isLengthOfDecodeBinaryStringMultipleOf7(inputMessage)
                             && is0Or00InEvenBlocks(inputMessage)) {
                         System.out.println("Decoded string:");
@@ -57,38 +57,41 @@ public class Main {
                 i++;
             }
 
-            result.append((currentElementOfBinary == '1' ? "0" : "00") + " " + "0".repeat(count)+ " ");
+            result.append((currentElementOfBinary == '1' ? "0" : "00") + " " + "0".repeat(count) + " ");
             i++;
         }
         return result.toString().trim();
     }
 
     public static String decodeInputMessage(String encodedMessage) {
-        StringBuilder decoded = new StringBuilder();
-
-        String[] blocks = encodedMessage.split(" ");
-        for (int i = 0; i < blocks.length; i += 2) {
-            int binaryDigit = blocks[i].equals("00") ? 0 : 1;
-            for (int j = 0; j < blocks[i + 1].length(); j++) {
-                decoded.append(binaryDigit == 0 ? '0' : '1');
-            }
-
-        }
+        String decodeBinary = decodingBinaryString(encodedMessage);
 
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < decoded.length(); i += 7) {
-            String binaryChar = decoded.substring(i, Math.min(i + 7, decoded.length()));
+        for (int i = 0; i < decodeBinary.length(); i += 7) {
+            String binaryChar = decodeBinary.substring(i, Math.min(i + 7, decodeBinary.length()));
             int decimalChar = Integer.parseInt(binaryChar, 2);
             result.append((char) decimalChar);
         }
         return result.toString();
     }
 
+    public static String decodingBinaryString(String encodedMessage) {
+        StringBuilder decoded = new StringBuilder();
+
+        String[] blocks = encodedMessage.split(" ");
+        for (int i = 0; i < blocks.length; i += 2) {
+            String binaryDigit = blocks[i].equals("00") ? "0" : "1";
+            int count = blocks[i + 1].length();
+            decoded.append(binaryDigit.repeat(count));
+        }
+        return decoded.toString();
+    }
+
     public static boolean isOnlyZeroOrSpaceInCode(String inputMessage) {
         return inputMessage.matches("^[0\\s]+$");
     }
 
-    public static boolean isNumberOfBlockEven (String inputMessage) {
+    public static boolean isNumberOfBlocksEven(String inputMessage) {
         String[] blocks = inputMessage.split(" ");
         int length = blocks.length;
         return (length % 2 == 0);
@@ -96,32 +99,16 @@ public class Main {
 
     public static boolean is0Or00InEvenBlocks(String inputMessage) {
         String[] blocks = inputMessage.split(" ");
-        boolean validBlock = true;
         for (int i = 0; i < blocks.length; i += 2) {
-            if ("0".equals(blocks[i]) || "00".equals(blocks[i])) {
-                validBlock = true;
-            } else {
-                validBlock = false;
+            if (!("0".equals(blocks[i]) || "00".equals(blocks[i]))) {
+                return false;
             }
         }
-        return validBlock;
-    }
-
-    public static int determineLengthOfDecodeBinaryString(String inputMessage) {
-        StringBuilder decoded = new StringBuilder();
-        String[] blocks = inputMessage.split(" ");
-
-        for (int i = 0; i < blocks.length; i += 2) {
-            int count = blocks[i].equals("00") ? 0 : 1;
-            for (int j = 0; j < blocks[i + 1].length(); j++) {
-                decoded.append(count == 0 ? '0' : '1');
-            }
-        }
-        return decoded.length();
+        return true;
     }
 
     public static boolean isLengthOfDecodeBinaryStringMultipleOf7(String inputMessage) {
-        int length = determineLengthOfDecodeBinaryString(inputMessage);
+        int length = decodingBinaryString(inputMessage).length();
         return length % 7 == 0;
     }
 }
